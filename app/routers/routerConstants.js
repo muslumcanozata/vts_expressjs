c8c4b6de0af6de3c2695c71916fec5bd4c0c4aff
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-12-26 20:45:47 +0300

    renaming directories

--- app/routers/routerConstants.js
+++ app/routers/routerConstants.js
@@ -1,9 +1,9 @@
 const API_PREFIX = "/api/v1/";
-const DEVICE_ROUTE = "device/";
+const DEVICE_ROUTE = "deviceRouter/";
 const DEVICE_TYPE_ROUTE = "deviceType/";
 const LOG_LOCATION_ROUTE = "logLocation/";
 const LOG_TEMPERATURE_ROUTE = "logTemperature/";
-const VEHICLE_ROUTE = "vehicle/";
+const VEHICLE_ROUTE = "vehicleRouter/";
 
 module.exports = {
     API_PREFIX,
--- server.js
+++ server.js
@@ -1,13 +1,9 @@
 const express = require("express")
-const app = express()
-const routes = require('./router/router')
+const app = express();
 const helmet = require("helmet");
 
-app.use(helmet())
-app.use(express.json())
-app.use(`${process.env.APP_PREFIX}user`, routes.user)
-app.use(`${process.env.APP_PREFIX}blog`, routes.blog)
-
+app.use(helmet());
+app.use(express.json());
 
 app.listen(process.env.APP_PORT, () => {
     console.log("Server Running" + " " + process.env.APP_PORT)
