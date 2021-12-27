52e7c0a29fe8ec013175a7b5bd4ecc6807dbf1f2
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-12-28 00:41:46 +0300

    adding routers and connecting postgres db

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-const express = require("express")
+const express = require("express");
 const app = express();
+const router = express.Router();
 const helmet = require("helmet");
+const routes = require('./app/routers/routes');
+const config = require('./app/configs/config');
+const db = require('./app/db/db');
+const { DEVICE_ROUTE, } = require('./app/routers/routerConstants');
+
+config.serverConfig.installServerConfigs();
 
 app.use(helmet());
 app.use(express.json());
+app.use(`${process.env.APP_PREFIX}${DEVICE_ROUTE}`, routes.deviceRouter);
 
 app.listen(process.env.APP_PORT, () => {
-    console.log("Server Running" + " " + process.env.APP_PORT)
+    db.postgresConnection.connectPostgres(
+        config.postgresConfig.db_username,
+        config.postgresConfig.db_password,
+        config.postgresConfig.db_host,
+        config.postgresConfig.db_port,
+        config.postgresConfig.db_name
+    );
+    console.log("Server Running " + process.env.APP_PORT);
 })
