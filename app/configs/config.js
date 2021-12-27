b7088aa9f067771f384ca43b69b78949c884bfcb
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-12-28 00:35:56 +0300

    creating configs layer

---
+++ app/configs/config.js
@@ -0,0 +1,8 @@
+const serverConfig = require('./serverConfig')
+const postgresConfig = require('./postgresConfig')
+
+
+module.exports = {
+    serverConfig: serverConfig,
+    postgresConfig: postgresConfig,
+}
