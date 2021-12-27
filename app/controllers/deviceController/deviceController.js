5450f9f911b7526ba945941149a007bb0323bc4b
Müslüm Can Özata <muslumcanozata@gmail.com>
2021-12-28 01:06:46 +0300

    bugfix

--- app/controllers/deviceController/deviceController.js
+++ app/controllers/deviceController/deviceController.js
@@ -1,12 +1,12 @@
-const deviceService = require('../../services/deviceService')
 const { StatusCodes } = require('http-status-codes')
 
 exports.getAll = (req, res, next) => {
-    try {
+    return res.send("OK");
+    /*try {
         const result = "RESULT";
         return res.send(result).status(StatusCodes.OK)
         //res.json(result).status(StatusCodes.OK)
     } catch (error) {
         return res.json({ error: error.message, code: "XYZ_101", timestamp: Date.now() }).status(StatusCodes.INTERNAL_SERVER_ERROR)
-    }
+    }*/
 }
