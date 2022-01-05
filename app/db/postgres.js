6f269c3ba26429bcee2a54df9a91284c3c376e35
Müslüm Can Özata <muslumcanozata@gmail.com>
2022-01-06 01:05:19 +0300

    modifying db connection initializer

--- app/db/postgres.js
+++ app/db/postgres.js
@@ -1,9 +1,12 @@
+const config = require("../configs/config");
 const pgp = require('pg-promise')(/* options */);
 
-exports.connectPostgres = async (username, password, host, port, db_name) => {
-    try{
-        await pgp(`postgres://${username}:${password}@${host}:${port}/${db_name}`);
-    } catch (error) {
-        throw new Error("Cannot Connect to PostgreSQL!"+error);
-    }
+let cn = `postgres://${config.postgresConfig.db_username}:${config.postgresConfig.db_password}@${config.postgresConfig.db_host}:${config.postgresConfig.db_port}/${config.postgresConfig.db_name}`;
+try {
+    const db = pgp(cn);
+    module.exports = { db };
+} catch(error) {
+    throw new Error("Cannot connect PostgreSQL");
 }
+
+
